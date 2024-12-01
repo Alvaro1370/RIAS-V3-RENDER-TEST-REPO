b69a9e1642aa3e63afe50d6b202730049b052d7e
Alvaro1370 <alvarosteel45@gmail.com>
2024-12-01 11:12:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     prefix: '.', // Set your command prefix
-    ownerName: 'Toxxic',
-    ownerNumber: '2348165846414', // Your WhatsApp number in international format
+    ownerName: 'DAVID',
+    ownerNumber: '2348109243618', // Your WhatsApp number in international format
     mode: 'private', // Default mode; can be 'public' or 'private'
     region: 'Nigeria', 
     botName: 'Rias Gremory V3', 
-    exifPack: 'RIAS V3 LOVES',  // The name of your sticker pack
-    exifAuthor: 'Toxxic',       // Your name or the author of the sticker pack
+    exifPack: '𝕵𝖊 𝖛𝖔𝖎𝖘 𝖘𝖙𝖎𝖈𝖐𝖊𝖗 𝖕𝖆𝖈𝖐',  // The name of your sticker pack
+    exifAuthor: '𝕯𝖆𝖛𝖎𝖉',       // Your name or the author of the sticker pack
     timeZone: 'Africa/Lagos',// Your Country Time Zone
     presenceStatus: 'recording', // Default bot presence status (available, typing, recording, invisible)
     autoRead: 'false', // true or false for autoread functions
